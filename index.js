edcbbc19496850e32b13aa58364b53b129bedd20
dannyfrelink <danny-frelink@hotmail.com>
2021-02-22 19:59:26 +0100

    toevoeging post aan index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('static/public'));
 app.engine('handlebars', expHandlebars());
 app.set('view engine', 'handlebars');
 
-app.get('/profiel-1', (req, res) => {
+app.get('/persoonsgegevens', (req, res) => {
     res.render('profiel-1');
 });
 
@@ -22,41 +22,53 @@ app.get('/fotos', (req, res) => {
     res.render('addFoto');
 });
 
-app.get('/profiel-2', (req, res) => {
+app.post('/fotos', (req, res) => {
     res.render('profiel-2');
 });
+
+// app.get('/profiel-2', (req, res) => {
+//     res.render('profiel-2');
+// });
 
 app.get('/zoekopdracht', (req, res) => {
     res.render('addZoekopdracht');
 });
 
-app.get('/profiel-3', (req, res) => {
+app.post('/zoekopdracht', (req, res) => {
     res.render('profiel-3');
 });
+
+// app.get('/profiel-3', (req, res) => {
+//     res.render('profiel-3');
+// });
 
 app.get('/reizen', (req, res) => {
     res.render('addReizen');
 });
 
-app.get('/profiel-4', (req, res) => {
+app.post('/reizen', (req, res) => {
     res.render('profiel-4', {landen});
 });
 
-app.get('/profiel/persoonsgegevens', (req, res) => {
-    res.render('Dit is de pagina waarop de persoonsgegevens worden ingevuld');
-});
+// app.get('/profiel-4', (req, res) => {
+//     res.render('profiel-4', {landen});
+// });
 
-app.get('/profiel/fotos', (req, res) => {
-    res.render('Dit is de pagina waarop de fotos toe te voegen zijn.');
-});
+// app.get('/profiel/persoonsgegevens', (req, res) => {
+//     res.render('Dit is de pagina waarop de persoonsgegevens worden ingevuld');
+// });
 
-app.get('/profiel/zoekopdracht', (req, res) => {
-    res.render('Dit is de pagina waarop je kan aangeven waar je naar op zoek bent om mee te matchen.');
-});
+// app.get('/profiel/fotos', (req, res) => {
+//     res.render('Dit is de pagina waarop de fotos toe te voegen zijn.');
+// });
 
-app.get('/profiel/reizen', (req, res) => {
-    res.render('Dit is de pagina waar je kan aangeven welke reizen je allemaal wilt maken.');
-});
+// app.get('/profiel/zoekopdracht', (req, res) => {
+//     res.render('Dit is de pagina waarop je kan aangeven waar je naar op zoek bent om mee te matchen.');
+// });
+
+// app.get('/profiel/reizen', (req, res) => {
+//     res.render('Dit is de pagina waar je kan aangeven welke reizen je allemaal wilt maken.');
+// });
 
 app.use(function (req, res, next) {
     res.status(404).send('Sorry, deze pagina kon ik niet vinden.')
