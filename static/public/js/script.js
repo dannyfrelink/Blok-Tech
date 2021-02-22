dadfef98056000428e707198ba482741a01ff3e9
dannyfrelink <danny-frelink@hotmail.com>
2021-02-22 18:39:10 +0100

    Fix JS foto uploader

--- static/public/js/script.js
+++ static/public/js/script.js
@@ -1,106 +1,51 @@
-var kiesImage = document.querySelectorAll('form.fotos input');
-var displayImage = document.querySelectorAll('form.fotos label');
+var checkboxLabels = document.querySelectorAll(`form.zoekopdracht>div>div label`);
+var chooseSelects = document.querySelectorAll(`form.zoekopdracht select`);
 
-var checkboxLabels = document.querySelectorAll('form.zoekopdracht>div>div label');
-var chooseSelects = document.querySelectorAll('form.zoekopdracht select ');
+var imageInputs = document.querySelectorAll(`form.fotos input`);
+var imageLabels = document.querySelectorAll(`form.fotos label`);
 
 checkboxLabels.forEach(function (checkboxLabel) {
-    checkboxLabel.addEventListener('click', function () {
-        checkboxLabel.classList.toggle('checked')
-    })
+    checkboxLabel.addEventListener(`click`, function () {
+        checkboxLabel.classList.toggle(`checked`);
+    });
 });
 
 chooseSelects.forEach(function (chooseSelect) {
-    chooseSelect.addEventListener('change', function () {
-        chooseSelect.classList.add('checked')
-    })
+    chooseSelect.addEventListener(`change`, function () {
+        chooseSelect.classList.add(`checked`);
+    });
 });
 
-kiesImage.addEventListener('change', function () {
-    getDataImg();
+imageInputs.forEach(function (imageInput) {
+    imageInput.addEventListener(`change`, function (e) {
+        displayImage(e);
+        displayLabel();
+    });
 });
 
-function getDataImg () {
-    var files1 = kiesImage[0].files[0];
-    var files2 = kiesImage[1].files[1];
-    var files3 = kiesImage[2].files[2];
-    var files4 = kiesImage[3].files[3];
-    var files5 = kiesImage[4].files[4];
-    var files6 = kiesImage[5].files[5];
-    var files7 = kiesImage[6].files[6];
-    var files8 = kiesImage[7].files[7];
-    var files9 = kiesImage[8].files[8];
+function displayImage (e) {
+    var labelElement = document.querySelector(`[for = "${e.target.id}"]`)
 
-    if (files1){
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(files1);
-        fileReader.addEventListener("load", function () {
-            displayImage[0].style.display = "block";
-            displayImage[0].innerHTML = '<img src="' + this.result + '" />'
+    var fileReader = new FileReader();
+    fileReader.readAsDataURL(e.target.files[0]);
+    fileReader.addEventListener(`load`, function () {
+        labelElement.innerHTML = `<img src="${this.result}" />`
+    });
+
+    e.target.addEventListener(`click`, function (e) {
+        e.preventDefault();
+    });
+}
+
+function displayLabel () {
+    var found = false
+
+    imageLabels.forEach(function(imageLabel, index) {
+        imageLabel.childNodes.forEach(function (childNode) {
+            if(childNode.tagName !== `IMG` && !found) {
+                found = true
+                imageLabels[index+1].style.display = 'block';
+            }
         });
-    }
-    if (files2) {
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(files2);
-        fileReader.addEventListener("load", function () {
-            displayImage[1].style.display = "block";
-            displayImage[1].innerHTML = '<img src="' + this.result + '" />'
-        });
-    };
-    if (files3){
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(files3);
-        fileReader.addEventListener("load", function () {
-            displayImage[2].style.display = "block";
-            displayImage[2].innerHTML = '<img src="' + this.result + '" />'
-        });
-    }
-    if (files4) {
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(files4);
-        fileReader.addEventListener("load", function () {
-            displayImage[3].style.display = "block";
-            displayImage[3].innerHTML = '<img src="' + this.result + '" />'
-        });
-    };
-    if (files5){
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(files5);
-        fileReader.addEventListener("load", function () {
-            displayImage[4].style.display = "block";
-            displayImage[4].innerHTML = '<img src="' + this.result + '" />'
-        });
-    }
-    if (files6) {
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(files6);
-        fileReader.addEventListener("load", function () {
-            displayImage[5].style.display = "block";
-            displayImage[5].innerHTML = '<img src="' + this.result + '" />'
-        });
-    };
-    if (files7){
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(files7);
-        fileReader.addEventListener("load", function () {
-            displayImage[6].style.display = "block";
-            displayImage[6].innerHTML = '<img src="' + this.result + '" />'
-        });
-    }
-    if (files8) {
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(files8);
-        fileReader.addEventListener("load", function () {
-            displayImage[7].style.display = "block";
-            displayImage[7].innerHTML = '<img src="' + this.result + '" />'
-        });
-    };
-    if (files9) {
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(files9);
-        fileReader.addEventListener("load", function () {
-            displayImage[8].style.display = "block";
-            displayImage[8].innerHTML = '<img src="' + this.result + '" />'
-        });
-    };
+    });
 }
