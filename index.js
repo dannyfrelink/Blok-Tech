c29e2bd9311429a7e896ecce6dd4e8fa760bc8e1
dannyfrelink <danny-frelink@hotmail.com>
2021-02-25 13:27:01 +0100

    opschoning index.js

--- index.js
+++ index.js
@@ -27,17 +27,9 @@ app.post('/fotos', (req, res) => {
     res.render('addFoto');
 });
 
-// app.get('/profiel/fotos', (req, res) => {
-//     res.render('profiel-2');
-// });
-
 app.post('/profiel/fotos', (req, res) => {
     res.render('profiel-2');
 });
-
-// app.get('/profiel-2', (req, res) => {
-//     res.render('profiel-2');
-// });
 
 app.get('/zoekopdracht', (req, res) => {
     res.render('addZoekopdracht');
@@ -47,10 +39,6 @@ app.post('/profiel/zoekopdracht', (req, res) => {
     res.render('profiel-3');
 });
 
-// app.get('/profiel-3', (req, res) => {
-//     res.render('profiel-3');
-// });
-
 app.get('/reizen', (req, res) => {
     res.render('addReizen');
 });
@@ -58,28 +46,6 @@ app.get('/reizen', (req, res) => {
 app.post('/profiel/reizen', (req, res) => {
     res.render('profiel-4', {landen});
 });
-
-// app.get('/profiel-4', (req, res) => {
-//     res.render('profiel-4', {landen});
-// });
-
-// app.get('/profiel/persoonsgegevens', (req, res) => {
-//     res.render('Dit is de pagina waarop de persoonsgegevens worden ingevuld');
-// });
-
-// app.get('/profiel/fotos', (req, res) => {
-//     res.render('Dit is de pagina waarop de fotos toe te voegen zijn.');
-// });
-
-// app.get('/profiel/zoekopdracht', (req, res) => {
-//     res.render('Dit is de pagina waarop je kan aangeven waar je naar op zoek bent om mee te matchen.');
-// });
-
-// app.get('/profiel/reizen', (req, res) => {
-//     res.render('Dit is de pagina waar je kan aangeven welke reizen je allemaal wilt maken.');
-// });
-
-
 
 app.use(function (req, res, next) {
     res.status(404).send('Sorry, deze pagina kon ik niet vinden.')
