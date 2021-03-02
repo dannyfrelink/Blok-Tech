408712b4b18d824e602dd755891a4e28a4ed4de9
dannyfrelink <danny-frelink@hotmail.com>
2021-03-02 23:25:28 +0100

    werkend maken van addReizen.handlebars

--- static/public/js/script.js
+++ static/public/js/script.js
@@ -6,6 +6,9 @@ const chooseSelects = document.querySelectorAll(`form.zoekopdracht select`);
 const imageInputs = document.querySelectorAll(`form.fotos input`);
 const imageLabels = document.querySelectorAll(`form.fotos label`);
 
+const continenten = document.querySelectorAll(`form.reizen>div:first-of-type label`);
+const landenContinenten = document.querySelectorAll(`form.reizen>div:nth-of-type(2) div`);
+
 checkboxLabels.forEach(function (checkboxLabel) {
 	checkboxLabel.addEventListener(`click`, function () {
 		checkboxLabel.classList.toggle(`checked`);
@@ -49,5 +52,42 @@ function displayLabel() {
 				imageLabels[index + 1].style.display = `block`;
 			}
 		});
+	});
+}
+
+for (let i = 0; i < landenContinenten.length; i++) {
+	continenten[0].addEventListener(`click`, function () {
+		landenContinenten[i].style.display = `none`;
+		landenContinenten[0].style.display = `flex`;
+	});
+
+	continenten[1].addEventListener(`click`, function () {
+		landenContinenten[i].style.display = `none`;
+		landenContinenten[1].style.display = `flex`;
+	});
+
+	continenten[2].addEventListener(`click`, function () {
+		landenContinenten[i].style.display = `none`;
+		landenContinenten[2].style.display = `flex`;
+	});
+
+	continenten[3].addEventListener(`click`, function () {
+		landenContinenten[i].style.display = `none`;
+		landenContinenten[3].style.display = `flex`;
+	});
+
+	continenten[4].addEventListener(`click`, function () {
+		landenContinenten[i].style.display = `none`;
+		landenContinenten[4].style.display = `flex`;
+	});
+
+	continenten[5].addEventListener(`click`, function () {
+		landenContinenten[i].style.display = `none`;
+		landenContinenten[5].style.display = `flex`;
+	});
+
+	continenten[6].addEventListener(`click`, function () {
+		landenContinenten[i].style.display = `none`;
+		landenContinenten[6].style.display = `flex`;
 	});
 }
