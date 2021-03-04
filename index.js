323d10c087201f31ddaee8f3af8ba7d348b47a43
dannyfrelink <danny-frelink@hotmail.com>
2021-03-04 14:39:08 +0100

    database verbinden

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const client = new MongoClient(dbURL, { useUnifiedTopology: true });
 const landen = Object.values(countriesList.countries);
 const continenten = Object.values(countriesList.continents);
 
+console.log(landen);
+
 const hbs = expHandlebars.create({
 	helpers: {
 		equals: (value1, value2) => { return value1 === value2; }
@@ -42,7 +44,7 @@ app.post(`/fotos`, async (req, res) => {
 		console.log(`Connectie gelukt`);
 		const collection = database.collection(`persoonsgegevens`);
 		const document = { "name": req.body.name, "nationaliteit": req.body.nation, "geboortedatum": req.body.geboorte, "geslacht": req.body.geslacht, "bio": req.body.bio };
-		await collection.insertOne({document});	
+		await collection.insertOne({ document });
 
 		persoonsgegevensDB = await collection.find({}).toArray();
 
@@ -66,12 +68,13 @@ app.post(`/profiel/fotos`, async (req, res) => {
 		await client.connect();
 		const database = await client.db(process.env.DB_CALL);
 		console.log(`Connectie gelukt`);
-		const collection = database.collection(`fotos`);
+		const fotoCollection = database.collection(`fotos`);
+		const persoonCollection = database.collection(`persoonsgegevens`);
 		const document = { "pfImage": req.body.pfImage.innerHTML, "extraImage1": req.body.extraImage1.innerHTML, "extraImage2": req.body.extraImage2.innerHTML, "extraImage3": req.body.extraImage3.innerHTML };
-		await collection.insertOne({document});	
+		await fotoCollection.insertOne({ document });
 
-		fotosDB = await collection.find({}).toArray();
-		persoonsgegevensDB = await collection(`persoonsgegevens`).find({}).toArray();
+		fotosDB = await fotoCollection.find({}).toArray();
+		persoonsgegevensDB = await persoonCollection.find({}).toArray();
 
 		console.log(fotosDB);
 		console.log(persoonsgegevensDB);
@@ -99,7 +102,7 @@ app.post(`/profiel/zoekopdracht`, async (req, res) => {
 		console.log(`Connectie gelukt`);
 		const collection = database.collection(`zoekopdracht`);
 		const document = { "geslacht": req.body.geslacht, "nationaliteit": req.body.nation, "leeftijd": req.body.leeftijd, "interesses": req.body.interesses };
-		await collection.insertOne({document});	
+		await collection.insertOne({ document });
 
 		zoekopdrachtDB = await collection.find({}).toArray();
 
@@ -111,7 +114,7 @@ app.post(`/profiel/zoekopdracht`, async (req, res) => {
 	finally {
 		await client.close();
 	}
-	res.render(`profiel-3`, {zoekopdrachtDB});
+	res.render(`profiel-3`, { zoekopdrachtDB });
 });
 
 app.get(`/reizen`, async (req, res) => {
@@ -126,7 +129,7 @@ app.post(`/profiel/reizen`, async (req, res) => {
 		const database = await client.db(process.env.DB_CALL);
 		console.log(`Connectie gelukt`);
 		const collection = database.collection(`landen`);
-		const gekozenLanden = { "landAF": req.body.landenAF.innerHTML };
+		const gekozenLanden = { "landAF": req.body.landenAF, "landAn": req.body.landenAN };
 		await collection.insertOne(gekozenLanden);
 
 		landenDB = await collection.find({}).toArray();
